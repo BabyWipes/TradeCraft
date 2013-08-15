7de51166ed555875d3bfe2d9c5b099cd8d31bf67
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-15 22:00:32 +1000

    Update Base.java

--- Base.java
+++ Base.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Base extends JavaPlugin {
@@ -19,37 +20,62 @@ public final class Base extends JavaPlugin {
 		getLogger().info("[Trade] Plugin disabled!");
 	}
 
+
+
 	public boolean onCommand(CommandSender caller, Command cmd,
 			String label, String[] args){
+
+		Player pcaller = (Player) caller; 
+
 		if(cmd.getName().equalsIgnoreCase("Trade"))
 			if(args.length != 3){ //If it's not /command arg0 arg1 arg2
 				caller.sendMessage(ChatColor.RED + "#Trade: " + ChatColor.BLUE + "Please " +
-						"use /trade <player> <amount> <price>");
+						ChatColor.BLUE + "use /trade <player> <amount> <price>");
 				return true;
 			}else{
 
 				String target = args[0];
 				Player p = Bukkit.getPlayer(target);
 
-				if(args.length == 3){ 
+				if(args.length == 3){ //If command is /command arg0 arg1 arg2
 
-					if(!p.isOnline()){
+					if(!p.isOnline()){ //If p (target) is not online
 						caller.sendMessage(ChatColor.RED + "#Trade: " + 
 								ChatColor.BLUE + p.getName() + ChatColor.BLUE + "Is not online!");
 					}
 				}else{
-					for(int i = 0; i > 32; caller.sendMessage(ChatColor.RED + "#Trade: " + 
-							ChatColor.BLUE + "You cannot trade more than 32 of an item at once")){
-						if(args[1].length() > i){
+
+					boolean number = true;
+
+					if(p.isOnline()){
+						try{
+							Integer.parseInt(args[1]);
+						}catch(NumberFormatException e){
+							p.sendMessage(ChatColor.RED + "#Trade: " +
+									ChatColor.BLUE + "Please use /trade <player> <amount> <price>");
+							number = false;
+						}
+
+						String getNumber = args[1];
+
+						int getAmount = pcaller.getInventory().getItemInHand().getAmount();
+
+						if(getAmount < Integer.getInteger(getNumber) ){
 							caller.sendMessage(ChatColor.RED + "#Trade: " +
-                                                    ChatColor.BLUE + "You cannot trade more than 32 of an item at once);
+									ChatColor.BLUE + "Incorrect amount!");
 						}else{
-                                            //TODO
-                                               }
+							if(getAmount > Integer.getInteger(getNumber) || getAmount == Integer.getInteger(getNumber)){
+
+							}
+						}
 					}
 				}
 			}
 
+
 		return false;
 	}
+
+
+
 }
